18b92be0f90e859486a92866356a81242f1f14d5
sriya <baraddhasriya0110@gmail.com>
2025-06-30 13:42:23 +0530

    Update basic_calculator.h

--- basic_calculator.h
+++ basic_calculator.h
@@ -1,5 +1,5 @@
 // basic_calculator.h
-#ifndef BASIC_CALCULATOR_H
+#ifndef BASIC_CALCULATOR_H  //using ifndef to avoid "redefinition" of the functions
 #define BASIC_CALCULATOR_H
 
 //functions declaration
