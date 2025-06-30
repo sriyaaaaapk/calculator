ae732710b36818e8757c84b1e8644e09c790951c
sriya <baraddhasriya0110@gmail.com>
2025-06-30 13:40:15 +0530

    Update scientific_calculator.h

--- scientific_calculator.h
+++ scientific_calculator.h
@@ -1,5 +1,16 @@
 // scientific_calculator.h
 #ifndef SCIENTIFIC_CALCULATOR_H
 #define SCIENTIFIC_CALCULATOR_H
+
+// Function declarations
+void power();
+void square_root();
+void sine();
+void cosine();
+void tangent();
+void arcsine();
+void arccosine();
+void arctangent();
+
 void scientific_calculator_menu();
 #endif
